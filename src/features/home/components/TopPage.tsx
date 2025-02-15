e6c3d54212b3bf946cf0008d1d729a37292e0826
k0kishima <56298669+k0kishima@users.noreply.github.com>
2025-02-15 12:23:20 +0900

    Add temporary top page

---
+++ src/features/home/components/TopPage.tsx
@@ -0,0 +1,9 @@
+const TopPage = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Blindfold Chess</h1>
+    </div>
+  );
+};
+
+export default TopPage; 
