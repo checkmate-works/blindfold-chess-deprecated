0191aeb6f618ac44564dc8f9fde9ebacb45c8d0a
k0kishima <56298669+k0kishima@users.noreply.github.com>
2025-05-04 13:15:07 +0900

    Configure i18n

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/app/router";
 import { Toaster } from "react-hot-toast";
+import "@/i18n/config";
 
 export const App = () => {
   return (
